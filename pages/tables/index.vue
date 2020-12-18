82bcf2f69ff932b7fd14d8ed21a5c50032bfa2b6
tadashimasuda <tmasu4260@gmail.com>
2020-12-18 22:37:20 +0900

    /api/tables　取得 closed #6

---
+++ pages/tables/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        {{tables}}
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            tables:[]
+        }
+    },
+    async asyncData({$axios}){
+        let { data } = await $axios.$get('/tables');
+        return{
+            tables:data
+        }
+    }
+}
+</script>
