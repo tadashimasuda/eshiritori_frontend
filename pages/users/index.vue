8e2ba97f8278400bb3cc86a742fe9c61925ff887
tadashimasuda <tmasu4260@gmail.com>
2020-12-18 22:30:15 +0900

    /api/usersのデータ取得

---
+++ pages/users/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="users">
+            <div class="user">
+                一覧：{{users}}
+                {{links}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            users:[],
+            links:''
+        }
+    },
+    async asyncData({$axios}){
+        let { data,links } = await $axios.$get('/users');
+        return{
+            users:data,
+            links:links
+        }
+    }
+}
+</script>
