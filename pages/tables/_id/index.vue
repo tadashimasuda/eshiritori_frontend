cbb042e6752834e8ea1973bfba97ff6ec2e8f435
tadashimasuda <tmasu4260@gmail.com>
2020-12-18 23:22:22 +0900

    /tables/${id} 作成　取得

---
+++ pages/tables/_id/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        {{tables}}
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            tables:[]
+        }
+    },
+    async asyncData({route,$axios}){
+        const id = route.params.id;
+        let { data } = await $axios.$get(`/tables/${id}`);
+        return{
+            tables:data
+        }
+    }
+}
+</script>
