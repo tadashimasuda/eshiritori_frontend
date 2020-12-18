4d4e9fd4e241a885a43590b2a7d288bef0954afb
tadashimasuda <tmasu4260@gmail.com>
2020-12-18 22:58:43 +0900

    個人ページ作成　fix #7

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="container">
+    <div>
+        user:{{user}}
+    </div>
+    <div>
+        posts:{{posts}}
+    </div>
+    <div>
+        tables:{{tables}}
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            userData:[],
+            posts:[],
+            tables:[]
+        }
+    },
+    async asyncData({route,$axios}){
+        const id = route.params.id;
+        let { data } = await $axios.$get(`/users/${id}`)
+        return{
+            userData:data.user,
+            posts:data.posts,
+            tables:data.tables,
+        }
+    }
+}
+</script>
