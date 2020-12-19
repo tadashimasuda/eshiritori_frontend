f94a6dd4707bcd06c24aba337ff1f6a1de82ac4c
tadashimasuda <tmasu4260@gmail.com>
2020-12-19 23:52:36 +0900

    テーブルページの編集

--- pages/tables/index.vue
+++ pages/tables/index.vue
@@ -1,6 +1,20 @@
 <template>
     <div class="container">
-        {{tables}}
+        <div class="cards mt-3 row mx-auto col-md-10">
+          <div class="card mr-2 mt-2" style="width: 18rem;" v-for="table in tables" :key="table.id">
+            <nuxt-link :to ="`/tables/${table.id}`">
+                <img :src="table.post.img_path" width="100%" height="180" alt="">
+            </nuxt-link>
+                <div class="pb-2">
+                    <h6 class="ml-2 mt-2 user_name">{{table.name}}</h6>
+                    <span class="ml-2">主催者：</span>
+                    <nuxt-link :to="`/users/${table.owner.id}`">
+                        <img :src="table.owner.img_path" width="22" height="22" alt="" >
+                        <span>{{table.owner.name}}</span>
+                    </nuxt-link>
+                </div>
+          </div>
+        </div>
     </div>
 </template>
 
