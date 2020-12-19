53b80e05374594f653164f5f847074fcc3cf4a5a
tadashimasuda <tmasu4260@gmail.com>
2020-12-19 12:14:29 +0900

    toppage横並びい

--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,24 @@
     <div class="contents mt-5">
       <div class="tables">
         <h2>最近更新されたテーブル</h2>
-        {{tables}}
+        <div class="tables cards d-flex flex-row">
+          <div class="table card mr-2" style="width: 18rem;" v-for="table in tables" :key="table.id">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect fill="#868e96" width="100%" height="100%"/><text fill="#dee2e6" dy=".3em" x="50%" y="50%">Image cap</text></svg>
+            <div class="card-body">
+              <p>{{table[0].name}}</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="posts">
+      <div class="posts mt-5">
         <h2>最新投稿</h2>
-        {{posts}}
+        <div class="posts cards d-flex flex-row">
+          <div class="post card mr-2" style="width: 18rem;" v-for="post in posts" :key="post.id"> 
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect fill="#868e96" width="100%" height="100%"/><text fill="#dee2e6" dy=".3em" x="50%" y="50%">Image cap</text></svg>
+            <div class="card-body">
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -39,5 +52,17 @@ export default {
 <style scoped>
 .top{
   height: 300px;
+}
+.cards{
+  overflow-x: auto;
+}
+.card{
+  flex: 0 0 300px;
+}
+.tables{
+  overflow-x: auto;
+}
+.{
+  flex: 0 0 300px;
 }
 </style>
