b209e17843300315219043922a173848c81983e8
tadashimasuda <tmasu4260@gmail.com>
2020-12-27 17:24:34 +0900

    css header&menubar

--- components/header.vue
+++ components/header.vue
@@ -1,52 +1,39 @@
 <template>
   <div>
-      <header class="bg-light">
-        <nav class="navbar navbar-light container">
-          <template v-if="authenticated">
-            <nuxt-link to='/oauth/twitter/redirect' class="navbar-brand text-left">ログイン</nuxt-link>
-          </template>
-          <template v-else>
-            <a href="#">name</a>
-          </template>
-          <nuxt-link to="/" class="navbar-brand mx-auto">Header</nuxt-link>
-          <div class="text-right">
-            <button type="button" @click="menu = !menu" class="navbar-toggler-icon border-0"></button>
+      <header>
+        <div id="header_container">
+          <div class="header_title">
+            <h1 id="title">えしりとり</h1>
           </div>
-        </nav>
+          <div id="menu_icon" @click="menu =!menu">
+            <i class="fas fa-bars fa-lg" v-if="!menu"></i>
+            <i class="fas fa-times fa-lg" v-if="menu"></i>
+          </div>
+        </div>
       </header>
-      <transition>
-        <div v-if="menu" class="drawer-menu-wrapper bg-light">
-          <div class="header_user text-center pt-4">
+      <transition name="menu">
+        <div v-if="menu" id="menu">
+          <div id="menu_user">
             <template v-if="user">
-              <img :src="user.img_path" alt="" class="user_img">
-              <h3 class="mt-4">{{user.name}}</h3>
+              <img :src="user.img_path" alt="ユーザー画像" id="menu_user_img">
+              <h3 id="menu_user_name">{{user.name}}</h3>
             </template>
           </div>
-          <div>
-            <ul class="m-0 p-0">
-              <li class="border py-2">
-                <nuxt-link to="/">
-                  <button class="btn" @click="menu = !menu">TOP</button>
-                </nuxt-link>
+          <div id="menus">
+            <ul>
+              <li id="menu_top" @click="menu = !menu">
+                <nuxt-link to="/" @click="menu = !menu" class="menu_link">TOP</nuxt-link>
               </li>
-              <li class="border py-2">
-                <nuxt-link to="/tables">
-                  <button class="btn" @click="menu = !menu">テーブル一覧</button>
-                </nuxt-link>
+              <li id="menu_tables" @click="menu = !menu">
+                <nuxt-link to="/tables" @click="menu = !menu" class="menu_link">テーブル一覧</nuxt-link>
               </li>
-              <li class="border py-2">
-                <nuxt-link to="/users">
-                  <button class="btn" @click="menu = !menu">画家一覧</button>
-                </nuxt-link>
+              <li id="menu_users" @click="menu = !menu">
+                <nuxt-link to="/users" @click="menu = !menu" class="menu_link">画家一覧</nuxt-link>
               </li>
-              <li class="border py-2">
-                <nuxt-link to="/oauth/twitter/redirect">
-                  <button class="btn" @click="menu = !menu">LOGIN</button>
-                </nuxt-link>
+              <li id="menu_login" @click="menu = !menu">
+                <nuxt-link to="/oauth/twitter/redirect" class="menu_link">LOGIN</nuxt-link>
               </li>
-              <li class="border py-2">
-                <button @click="deleteToken(); menu = !menu" class="btn">LOGOUT</button>
-              </li>
+              <li id="menu_logout" @click="deleteToken(); menu = !menu" class="menu_link">LOGOUT</li>
             </ul>
           </div>
         </div>
@@ -59,42 +46,9 @@ import { mapMutations } from 'vuex'
 export default {
   data(){
     return {
-      menu:false
+      menu:true
     }
   },
   methods: mapMutations(['deleteToken']),
 }
 </script>
-
-<style>
-.right-enter-active,
-.right-leave-active {
-  transform: translate(0px, 0px);
-  transition: transform 225ms cubic-bezier(0, 0, 0.2, 1) 0ms;
-}
-.right-enter,
-.right-leave-to {
-  transform: translateX(-100vw) translateX(0px);
-}
-.drawer-menu-wrapper {
-  position: absolute;
-  z-index: 10;
-  top: 0;
-  left: 0;
-  width: 50%;
-  height: 120%;
-}
-.header_user{
-  height: 150px;
-}
-ul{
-  list-style: none;
-}
-li:hover{
-  background-color: black;
-  opacity: 0.3;
-}
-.btn{
-  width: 100%;
-}
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="top border">
+    <div class="top">
     </div>
     <div class="contents mt-5">
       <div class="tables">
@@ -9,7 +9,6 @@
           <div class="table card mr-2" style="width: 18rem;" v-for="table in tables" :key="table.id">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect fill="#868e96" width="100%" height="100%"/><text fill="#dee2e6" dy=".3em" x="50%" y="50%">Image cap</text></svg>
             <div class="card-body">
-              <!-- <p>{{table[0].name}}</p> -->
             </div>
           </div>
         </div>
