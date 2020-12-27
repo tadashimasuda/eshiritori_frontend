77fa5e1dd0a06f59e31d2955b323adbcd9b6d79e
tadashimasuda <tmasu4260@gmail.com>
2020-12-27 23:05:41 +0900

    user posts

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,10 +1,10 @@
 <template>
-<div class="container">
-    <div class="col-md-8 border mx-auto">
-        <div class="user text-center mt-5">
-            <img :src="userData.img_path" alt="">
+<div class="user_wapper">
+    <div class="user_inner">
+        <div id="user_user_profile">
+            <img :src="userData.img_path" alt="" class="user_user_img">
             <h4 class="user_name mt-4">{{userData.name}}</h4>
-            <template v-if="userData.profile">
+            <!-- <template v-if="userData.profile">
                 <h5>プロフィール：</h5>
                 <pre class="user_profile">{{userData.profile}}</pre>
             </template>
@@ -12,23 +12,22 @@
                 <a :href="`http://twitter.com/${userData.twitter_id}`" target="_blank">
                 <i class="fab fa-twitter-square fa-2x"></i>
             </a>
-            </template>
+            </template> -->
         </div>
-    <div>
-    <div class="contents mt-4">
+    <div class="contents">
         <b-tabs content-class="mt-3" justified>
             <b-tab title="投稿された絵" active>
-                <div class="posts cards">
-                    <div class="post card mx-auto mt-3" style="width: 18rem;" v-for="post in posts" :key="post.id">
+                <div class="user_posts">
+                    <div class="user_post" v-for="post in posts" :key="post.id">
                         <nuxt-link :to="`/tables/${post.table_id}`">
-                            <img :src="`https://eshiritori-s3.s3-ap-northeast-1.amazonaws.com/post/${post.img_path}`" width="100%" height="180" alt="">
+                            <img :src="`https://eshiritori-s3.s3-ap-northeast-1.amazonaws.com/post/${post.img_path}`" alt="">
                         </nuxt-link>
                     </div>
                 </div>
             </b-tab>
             <b-tab title="主催しているテーブル">
-                <div class="tables cards">
-                    <div class="table card mx-auto mt-3" style="width: 18rem;" v-for="table in tables" :key="table.id">
+                <!-- <div class="tables cards">
+                    <div class="table card" style="width: 18rem;" v-for="table in tables" :key="table.id">
                         <img :src="`https://eshiritori-s3.s3-ap-northeast-1.amazonaws.com/post/${table.post.img_path}`" width="100%" height="180" alt="">
                         <div class="card-body m-0 p-0 pb-1">
                             <h4 class="mt-1 ml-1">{{table.name}}</h4>
@@ -37,13 +36,13 @@
                             <small>{{table.owner.name}}</small>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </b-tab>
         </b-tabs>
     </div>
     </div>
 </div>
-</div>
+
 
 </template>
 
