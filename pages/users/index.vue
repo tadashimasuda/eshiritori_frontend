33e5ea5f1ddcd2128abf14f1ac4771d671457aab
tadashimasuda <tmasu4260@gmail.com>
2020-12-27 20:43:29 +0900

    users page

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="container">
-        <div class="users col-md-6 mx-auto mt-3">
-            <div class="user border mb-2" v-for="user in users" :key="user.id">
+    <div class="wapper">
+        <h3 id="users_title">画家一覧</h3>
+        <div id="users">
+            <div class="user" v-for="user in users" :key="user.id">
                 <nuxt-link :to ="`/users/${user.id}`">
                     <span><img :src="user.img_path" height="50" width="50" alt=""></span>
                     <span>{{user.name}}</span>
