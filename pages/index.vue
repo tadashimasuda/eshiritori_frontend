2e1dea0f7f0a310f7fa5bd68e24a83325c8a8dd2
tadashimasuda <tmasu4260@gmail.com>
2021-02-03 00:44:53 +0900

    toppage edit recent posts card

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="wapper">
     <div id="top">
       <h2>Twitterで絵しりとりできるサービス</h2>
       <div class="twitter-login-btn">
@@ -7,43 +7,26 @@
         <span>Twitterでログイン</span>
       </div>
     </div>
-    <div class="wapper">
-      <div class="main">
-        <h3 class="top_main_title">最近投稿された絵</h3>
-        <div id="top_posts">
-          <div class="top_post"  v-for="post in posts" :key="post.id">
-            <div class="post_image">
-              <img :src="`https://eshiritori-s3.s3-ap-northeast-1.amazonaws.com/post/${post.img_path}`" alt="テーブル画像">
-            </div>
-            <div class="post_user">
-              <div class="user_img">
-                <img :src="`${post.user.img_path}`" alt="">
-                <nuxt-link to="/" class="user_name">{{post.user.name}}</nuxt-link>
-              </div>
-              <p>12月12日</p>
-            </div>
+    <div class="main">
+      <h3 id="top_main_title">最近投稿された絵</h3>
+      <div id="top_posts">
+        <div class="top-post"  v-for="post in posts" :key="post.id">
+          <div class="post-image">
+            <img :src="`https://eshiritori-s3.s3-ap-northeast-1.amazonaws.com/post/${post.img_path}`" alt="テーブル画像">
           </div>
-        </div>
-        <h3 class="top_main_title">最近更新されたテーブル</h3>
-        <div id="top_tables">
-        <div class="top_table" v-for="table in tables" :key="table.id">
-            <div class="post_image">
-              <img :src="`https://eshiritori-s3.s3-ap-northeast-1.amazonaws.com/post/${table.post.img_path}`" alt="テーブル画像">
+          <div class="post-user">
+            <div class="user-img">
+              <img :src="`${post.user.img_path}`" alt="">
             </div>
-            <p>{{table.name}}</p>
-            <div class="post_user">
-              <div class="user_img">
-                <img :src="`${table.owner.img_path}`" alt="">
-                <nuxt-link to="/" class="user_name">主催者：{{table.owner.name}}</nuxt-link>
-              </div>
-              <p>12月12日</p>
+            <div class="user-name">
+                <nuxt-link to="/" >{{post.user.name}}</nuxt-link>
             </div>
+            <p id="post_created_at">{{post.created_at}}</p>
           </div>
         </div>
       </div>
+    </div>
   </div>
-</div>
-  
 </template>
 
 <script>
@@ -59,6 +42,7 @@ export default {
       $axios.$get('/tables',{params:{'top':'top'}}),
       $axios.$get('/posts',{params:{'top':'top'}})
     ]);
+    console.log(postsData);
     return{
       tables:tablesData.data,
       posts:postsData.data
