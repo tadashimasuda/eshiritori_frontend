872d002587963f5a5ac5c6b6fe998f795abf5dd1
tadashimasuda <tmasu4260@gmail.com>
2021-02-07 23:13:25 +0900

    table index.vue design

--- pages/tables/index.vue
+++ pages/tables/index.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="tables_wapper">
-        <h3 id="tables_title">テーブル一覧</h3>
+    <div id="tables_wapper">
+        <!-- <h3 id="tables_title">テーブル一覧</h3> -->
         <div id="tables">
             <div id="table" v-for="table in tables" :key="table.id">
-                <div class="post_image">
-                <img :src="`https://eshiritori-s3.s3-ap-northeast-1.amazonaws.com/post/${table.post.img_path}`" alt="テーブル画像">
+                <div class="post-image">
+                    <img :src="`https://eshiritori-s3.s3-ap-northeast-1.amazonaws.com/post/${table.post.img_path}`" alt="テーブル画像">
                 </div>
                 <p>{{table.name}}</p>
-                <div class="post_user">
-                <div class="user_img">
-                    <img :src="`${table.owner.img_path}`" alt="">
-                    <nuxt-link to="/" class="user_name">主催者：{{table.owner.name}}</nuxt-link>
-                </div>
-                <p>12月12日</p>
+                <div class="post-user">
+                    <div class="user-img">
+                        <img :src="`${table.owner.img_path}`" alt="">
+                        <nuxt-link to="/" class="user-name">主催者：{{table.owner.name}}</nuxt-link>
+                    </div>
+                    <p>{{table.created_at}}</p>
                 </div>
             </div>
             <div class="cb"></div>
@@ -29,7 +29,6 @@ export default {
     },
     async asyncData({$axios}){
         let { data } = await $axios.$get('/tables');
-        console.log(data);
         return{
             tables:data
         }
