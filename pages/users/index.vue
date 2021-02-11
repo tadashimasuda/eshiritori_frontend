95336e2587e0901fb263bfb10ab05751be82823d
tadashimasuda <tmasu4260@gmail.com>
2021-02-11 19:48:16 +0900

    ユーザーネームの色変更

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="wapper">
-        <h3 id="users_title">画家一覧</h3>
+        <h3 id="users_title"><i class="fas fa-users users-title-icon"></i>画家一覧</h3>
         <div id="users">
             <div class="user" v-for="user in users" :key="user.id">
                 <nuxt-link :to ="`/users/${user.id}`">
                     <span><img :src="user.img_path" height="50" width="50" alt=""></span>
-                    <span>{{user.name}}</span>
+                    <span class="users-user-name">{{user.name}}</span>
                 </nuxt-link>
             </div>
         </div>
