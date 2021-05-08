c7b17385022ca07ea5cbb14e730c8a7b1344e51c
tadashimasuda <tmasu4260@gmail.com>
2021-05-08 21:55:17 +0900

    add error page fix #27

---
+++ layouts/error.vue
@@ -0,0 +1,8 @@
+<template>
+    <div id="errorpage" class="col-md-6 mx-auto">
+        <h4 class="text-center">ERROR</h4>
+        <p class="text-center">
+            <nuxt-link to="/" class="text-primary">TOPへ戻る</nuxt-link>
+        </p>
+    </div>
+</template>
