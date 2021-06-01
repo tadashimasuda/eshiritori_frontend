e1f30166fefb6ae982d6133b8715102cbe1b9818
tadashimasuda <tmasu4260@gmail.com>
2021-06-01 12:28:56 +0900

    cookieのエラー修正

--- store/index.js
+++ store/index.js
@@ -50,7 +50,7 @@ export const mutations = {
 
 export const actions = {
   async nuxtServerInit({ commit },{ req }){
-    if(cookieparser.parse(req.headers.cookie).token){
+    if(req && req.headers && req.headers.cookie){
       const token = 'Bearer ' + cookieparser.parse(req.headers.cookie).token
       let user = ''; 
       try {
